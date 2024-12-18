f3884bbe9f36d20c240130f4572e1e6722ed657b
Jay Barbhaiya <jay.barbhaiya@sap.com>
2024-12-18 15:32:10 +0100

    set catppuccin as colorscheme

--- init.lua
+++ init.lua
@@ -822,14 +822,14 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'bluz71/vim-moonfly-colors',
-    name = 'moonfly',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
-      vim.cmd.colorscheme 'moonfly'
+      vim.cmd.colorscheme 'catppuccin-mocha'
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 
