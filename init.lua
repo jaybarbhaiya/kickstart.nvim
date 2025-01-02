17adeed6325cc318aaa5a4f3445976c06e4772d4
Jay Barbhaiya <jay.barbhaiya@sap.com>
2025-01-02 10:30:24 +0100

    catppuccin-latte

--- init.lua
+++ init.lua
@@ -838,7 +838,7 @@ require('lazy').setup({
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'catppuccin-latte'
       -- You can configure highlights by doing something like:
       -- vim.cmd.hi 'Comment gui=none'
     end,
