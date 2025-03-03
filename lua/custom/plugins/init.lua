007ad1134b5d7b8f3450d139790ee25b55ca3674
Jay Barbhaiya <jay.barbhaiya@sap.com>
2025-03-03 16:06:20 +0100

    add bufferline

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -81,4 +81,13 @@ return {
       vim.cmd.colorscheme 'vscode'
     end,
   },
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      vim.opt.termguicolors = true
+      require('bufferline').setup {}
+    end,
+  },
 }
