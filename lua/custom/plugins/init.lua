090eecbfc3446042576d621e9df550dc9c13bf20
Jay Barbhaiya <jay.barbhaiya@sap.com>
2025-03-03 16:00:56 +0100

    add CopilotChat and refactor colorscheme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -16,10 +16,69 @@ return {
     'github/copilot.vim',
   },
   {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+  {
     'nvim-java/nvim-java',
     config = function()
       require('java').setup()
       require('lspconfig').jdtls.setup {}
+    end,
+  },
+  {
+    -- colorscheme
+    'Mofiqul/vscode.nvim',
+    config = function()
+      -- For dark theme (neovim's default)
+      vim.o.background = 'dark'
+      -- For light theme
+      -- vim.o.background = 'light'
+
+      local c = require('vscode.colors').get_colors()
+      require('vscode').setup {
+        -- Alternatively set style in setup
+        -- style = 'light'
+
+        -- Enable transparent background
+        transparent = true,
+
+        -- Enable italic comment
+        italic_comments = true,
+
+        -- Underline `@markup.link.*` variants
+        underline_links = true,
+
+        -- Disable nvim-tree background color
+        disable_nvimtree_bg = true,
+
+        -- Apply theme colors to terminal
+        terminal_colors = true,
+
+        -- Override colors (see ./lua/vscode/colors.lua)
+        color_overrides = {
+          vscLineNumber = '#FFFFFF',
+        },
+
+        -- Override highlight groups (see ./lua/vscode/theme.lua)
+        group_overrides = {
+          -- this supports the same val table as vim.api.nvim_set_hl
+          -- use colors from this colorscheme by requiring vscode.colors!
+          Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+        },
+      }
+      -- require('vscode').load()
+
+      -- load the theme without affecting devicon colors.
+      vim.cmd.colorscheme 'vscode'
     end,
   },
 }
